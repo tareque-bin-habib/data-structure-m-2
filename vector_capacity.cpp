da5a7856439d95dd1d934dcf5b07765ea954d0ad
tareque-bin-habib <shad.tbh@gmail.com>
2023-06-06 13:38:14 +0600

    first commit

---
+++ vector_capacity.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    // cout << v.max_size() << endl;
+    // cout << v.capacity() << endl;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(560);
+    // v.clear();
+    v.resize(2);
+    v.resize(7, 100);
+    cout << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
---
+++ vector_initialization.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v;  Type 1
+    // vector<int> v(5); Type 2
+    // vector<int> v(5, 10); Type 3
+    // vector<int> v2(5, 100); type 4
+    // vector<int> v(v2); type 4
+    // int a[6] = {1, 2, 3, 4, 5, 6};Type 5
+    // vector<int> v(a, a + 6); type 5
+
+    vector<int> v = {2, 10, 3};
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    cout << v.size() << endl;
+    return 0;
+}
